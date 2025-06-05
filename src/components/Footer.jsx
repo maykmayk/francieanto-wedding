ec47e56298b167c6b288c20c642ae10ad620de64
Michele de Vita <michele@quantum-studio.it>
2025-06-05 15:25:00 +0200

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,19 @@ const Footer = () => {
     <footer className="bg-sage text-white py-12">
       <div className="container">
         <div className="text-center mb-8">
-          <h2 className="font-custom text-3xl mb-2">Francesca & Antonio</h2>
+          {/* <h2 className="font-custom text-3xl mb-2">Francesca & Antonio</h2> */}
+          <div className="flex flex-col md:flex-row justify-center w-full items-center md:gap-1">
+            <h1 className="text-4xl md:text-6xl lg:text-7xl font-custom font-light md:mb-2">
+              Francesca
+            </h1>
+            <img className='w-10 md:w-16' src="/e.png" alt="and" />
+            <h1 className="text-4xl md:text-6xl lg:text-7xl font-custom font-light md:mb-2 mb-8">
+              Antonio
+            </h1>
+          </div>
           <p className="text-cream text-opacity-80 font-light">4 Ottobre 2025</p>
         </div>
-        
+
         <div className="flex justify-center space-x-6 mb-8">
           <a href="#" className="text-white hover:text-cream transition-colors duration-300">
             <FaInstagram size={24} />
@@ -16,7 +25,7 @@ const Footer = () => {
             <FaFacebook size={24} />
           </a>
         </div>
-        
+
         {/* <div className="max-w-md mx-auto mb-8">
           <nav>
             <ul className="flex flex-wrap justify-center gap-x-6 gap-y-2 text-sm">
@@ -83,7 +92,7 @@ const Footer = () => {
             </ul>
           </nav>
         </div> */}
-        
+
         <div className="text-center text-sm text-cream text-opacity-70">
           <p className="flex items-center justify-center">
             Creato con <FaHeart className="mx-1 text-red-400" /> da Mike
